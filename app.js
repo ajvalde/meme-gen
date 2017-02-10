6120599226553cd49d2dfb81f6fc8acff7a22b29
Benjamin Matson <benjaminmatson@gmail.com>
2017-02-10 13:27:28 -0700

    updated data bindings

--- app.js
+++ app.js
@@ -8,11 +8,18 @@
     })
 
     .component('creator', {
+      bindings: {
+        post: '=',
+        onSave: '&'
+      },
       templateUrl: 'creator.html',
       controller: creator_controller
     })
 
     .component('browser', {
+      bindings: {
+        post: '<'
+      },
       templateUrl: 'browser.html',
       controller: browser_controller
     })
@@ -21,7 +28,23 @@
     const vm = this
 
     vm.$onInit = function $onInit() {
+      vm.maxPostId = 3
+      vm.meme = {}
+      vm.memes = [{
+        id: 1,
+        image_url: '',
+        top_text: '',
+        bot_text: '',
+        timestamp: new Date(),
+        creator: ''
+      }]
+    }
 
+    vm.addMeme = function addMeme(savedMeme) {
+      ++vm.maxMemeId
+      savedMeme.id = vm.maxMemeId
+      savedMeme.timestamp = new Date()
+      vm.memes.push(savedMeme)
     }
 
   }
@@ -55,7 +78,7 @@
           image_url: "public/images/disaster-girl.jpg"
         },
         {
-          image_url: "public/images/disaster-girl.jpg"
+          image_url: "public/images/doge.jpg"
         },
         {
           image_url: "public/images/first-world-problems.jpg"
