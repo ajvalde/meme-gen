0dcf6b6ee42b82c37728792834a7116647ba8dea
Anthony <anthonyguitarman13@gmail.com>
2017-02-10 11:55:06 -0700

    images

--- app.js
+++ app.js
@@ -8,6 +8,55 @@ angular.module('app', [])
  function controller(){
    const vm = this
 
+   vm.images = [
+     {
+       image_url: "public/images/ancient-aliens.jpg"
+     },
+     {
+       image_url: "public/images/bad-luck-brian.jpg"
+     },
+     {
+       image_url: "public/images/batman-slapping-robin.jpg"
+     },
+     {
+       image_url: "public/images/but-thats-none-of-my-business.jpg"
+     },
+     {
+       image_url: "public/images/captain-picard-facepalm.jpg"
+     },
+     {
+       image_url: "public/images/confession-bear.jpg"
+     },
+     {
+       image_url: "public/images/creepy-condescending-wonka.jpg"
+     },
+     {
+       image_url: "public/images/disaster-girl.jpg"
+     },
+     {
+       image_url: "public/images/disaster-girl.jpg"
+     },
+     {
+       image_url: "public/images/first-world-problems.jpg"
+     },
+     {
+       image_url: "public/images/futurama-fry.jpg"
+     },
+     {
+       image_url: "public/images/one-does-not-simply.jpg"
+     },
+     {
+       image_url: "public/images/scumbag-steve.jpg"
+     },
+     {
+       image_url: "public/images/scumbag-steve.jpg"
+     },
+     {
+       image_url: "public/images/scumbag-steve.jpg"
+     }
+
+   ]
+
    vm.$onInit = function(){
 
    }
